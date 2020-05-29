685c9e68346dd8a85527ad33716c439def425320
Shakore McGill <mcgills2093@gmail.com>
2020-05-29 14:14:19 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,24 +1,25 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
+  new_cart = []
   coupons.each do |coupon_items|
     cart.each do |cart_items|
       if coupon_items[:item] == cart_items[:item]
         if cart_items[:count]>= coupon_items[:num]
-          cart_index = cart.length
           new_count = cart_items[:count] - coupon_items[:num]
           cart_items[:count] = new_count
-          cart[cart_index] = {
+          cart.length = {
             :item => cart_items[:item] + " W/COUPON",
             :price => coupon_items[:cost] / coupon_items[:num],
             :clearance => cart_items[:clearance],
             :count => coupon_items[:num]
           }
+          new_cart << cart[cart_index]
         end
       end
     end
   end
-  cart
+  new_cart
 end
 
 def apply_clearance(cart)
