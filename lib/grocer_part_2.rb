049e4e0369a7b4adb68d936cf0c23d5fe17eefc7
Shakore McGill <mcgills2093@gmail.com>
2020-05-29 14:32:23 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -34,13 +34,12 @@ def checkout(cart, coupons)
  consolidated_cart = consolidate_cart(cart)
  couponed_items = apply_coupons(consolidate_cart)
  final_cart = apply_clearance(couponed_items)
- 
  total = 0 
   final_cart.each do |cart_items|
     total += cart_items[:price] * cart_items[:count]
   end
   if total > 100
-    total -= (total - 0.10)
+    total -= (total * 0.10)
   end
   total
 end
